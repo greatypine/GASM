87822dcc0ce44dc08bf8cff2cc88ccfa1f2b3535
zhaoxuguang <zhaoxuguang@guoanshequ.com>
2018-05-17 09:14:05 +0800

    修改GASM发布脚本

--- deploy4release.sh
+++ deploy4release.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 command_str="ant "
-file_name="daqWeb.war"
+file_name="GASM.war"
 para_str="-Ddeploy=product -DwarFileName=$file_name"
 cur_dir=`pwd`
-work_dir="/root/daqWeb"
+work_dir="/root/GASM"
 dest_file="$cur_dir/$file_name"
 
 
 if [ -f $dest_file ]; then
-    echo "daqWeb.war already exists.overwrite file? Y/n"
+    echo "GASM.war already exists.overwrite file? Y/n"
     read ow
     if [ "$ow" == "Y" -o "$ow" == "y" -o "$ow" == "" ]; then
         rm -f $dest_file
--- deploy4test.sh
+++ deploy4test.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 command_str='ant '
-file_name="daqWeb.war"
+file_name="GASM.war"
 para_str="-Ddeploy=develop -DwarFileName=$file_name"
 cur_dir=`pwd`
-work_dir="/root/daqWeb"
+work_dir="/root/GASM"
 dest_file="$cur_dir/$file_name"
 
-cd /root/daqWeb
+cd /root/GASM
 if [ -f $dest_file ]; then
-    echo "daqWeb.war already exists.overwrite file? Y/n"
+    echo "GASM.war already exists.overwrite file? Y/n"
     read ow
     if [ "$ow" == "Y" -o "$ow" == "y" -o "$ow" == "" ]; then
         rm -f $dest_file
@@ -19,26 +19,27 @@ if [ -f $dest_file ]; then
 fi
 
 echo 'updating source...'
-svn up
+git pull origin master
 
 echo 'making warfile...'
-if /bin/sh $command_str$para_str; then
+if  $command_str$para_str; then
     if [ "$work_dir/$file_name" != $dest_file ]; then
         mv "$work_dir/$file_name" $dest_file
     fi
-    counter=$(ps -C java --no-heading | wc -l)
+    counter=`ps -ef | grep tomcat7gasm|grep -vE 'grep|tail'|wc -l`
     if [ $counter -eq 1 ];then
         echo 'stoping tomcat...'
-        /usr/local/tomcat7/bin/catalina.sh stop;
+        /usr/local/tomcat7gasm/bin/catalina.sh stop
+        sleep 5
     fi
-
+  
     echo 'deploying...'
-    rm -rf /usr/local/tomcat7/webapps/daqWeb* &&
-    cp $dest_file /usr/local/tomcat7/webapps/;
+    rm -rf /usr/local/tomcat7gasm/webapps/GASM* &&
+    cp $dest_file /usr/local/tomcat7gasm/webapps/
     echo 'starting tomcat...'
-    /usr/local/tomcat7/bin/catalina.sh start;
+    /usr/local/tomcat7gasm/bin/catalina.sh start
     sleep 5
-    counter=$(ps -C java --no-heading|wc -l)
+    counter=`ps -ef | grep tomcat7gasm|grep -vE 'grep|tail'|wc -l`
     if [ $counter -eq 1 ];then
         echo 'starting tomcat success...'
     else
