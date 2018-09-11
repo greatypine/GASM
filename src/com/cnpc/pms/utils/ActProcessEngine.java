142261a876063b5698314155a1fb9b2bcd8fe219
zhaoxuguang <zhaoxuguang@guoanshequ.com>
2018-09-11 09:45:49 +0800

    打开发布三个流程图

--- src/com/cnpc/pms/utils/ActProcessEngine.java
+++ src/com/cnpc/pms/utils/ActProcessEngine.java
@@ -58,9 +58,9 @@ public class ActProcessEngine {
 	        
 	        
 	        //上线前要发布一下。
-	        //repositoryService.createDeployment().addClasspathResource("actconf/oneVacationProcess.bpmn").name("请假流程1").category("请假1").deploy();
-	        //repositoryService.createDeployment().addClasspathResource("actconf/threeVacationProcess.bpmn").name("请假流程2").category("请假2").deploy();
-	        //repositoryService.createDeployment().addClasspathResource("actconf/moreVacationProcess.bpmn").name("请假流程3").category("请假3").deploy();
+	        repositoryService.createDeployment().addClasspathResource("actconf/oneVacationProcess.bpmn").name("请假流程1").category("请假1").deploy();
+	        repositoryService.createDeployment().addClasspathResource("actconf/threeVacationProcess.bpmn").name("请假流程2").category("请假2").deploy();
+	        repositoryService.createDeployment().addClasspathResource("actconf/moreVacationProcess.bpmn").name("请假流程3").category("请假3").deploy();
 	       
 	        
 	        ///////////////以下代码为测试///////////////
